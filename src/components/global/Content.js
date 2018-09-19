5b06f9c981636b1c758853a08b2b8ec74eee4833
sandraortiz <rocio12vx@gmail.com>
2018-09-19 18:49:22 -0500

    creando nuevos componentes

---
+++ src/components/global/Content.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './images/logo.svg';
+import './css/Content.css';
+// import Form from './Form'
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="Content">
+        <div className="Logo">
+         
+          <h1 >soy el contenedor</h1>
+          <p> loremjuhsfdafja </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Content;
---
+++ src/components/global/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './images/logo.svg';
+import './css/Header.css';
+// import Form from './Form'
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <div className="Logo">
+          <img src={logo}  alt="logo" />
+          <h1 >Welcome to React</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
