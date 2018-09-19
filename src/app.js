b9982d121872f4a714355cfffdb2138203bdc10e
Lizethsuk <julia.sucasacai@gmail.com>
2018-09-19 18:40:01 -0500

    instalando bootstrap

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Form from './Form'
+import Form from './Form';
+import { Grid } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+
+
+
 
 class App extends Component {
   render() {
@@ -12,9 +18,27 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Grid>
+            <Row className="show-grid">
+              <Col xs={12} md={6}>
+                Soy una columna!!!
+          </Col>
+              <Col xs={12} md={6}>
+                Columna2
+          </Col>
+              <Col xs={12} md={6}>
+                Columna3
+          </Col>
+              <Col xs={12} md={6}>
+                columna4
+          </Col>
+            </Row>
+          </Grid>
+          
         </p>
-        <Form inicial="hola mundo desde afuera"/>
+       
+
+        <Form inicial="hola mundo desde afuera" />
       </div>
     );
   }
