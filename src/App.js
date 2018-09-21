2b311577796b26c58d5292dd97e89ca64a10f74e
sandraortiz <rocio12vx@gmail.com>
2018-09-21 18:40:40 -0500

    prueba

--- src/App.js
+++ src/App.js
@@ -1,39 +1,21 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import Fire from './config/Fire';
-import Home from './home';
-import Login from './login';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = ({
-      user: null,
-    });
-    this.authListener = this.authListener.bind(this);
-  }
-  componentDidMount() {
-    this.authListener();
-  }
-
-  authListener() {
-    Fire.auth().onAuthStateChanged((user) => {
-      console.log(user);
-      if (user) {
-        this.setState({ user });
-        localStorage.setItem('user', user.uid);
-      } else {
-        this.setState({ user: null });
-        localStorage.removeItem('user');
-      }
-    });
-  }
   render() {
     return (
-     <div>{this.state.user ?  ( <Home/>) : (<Login />)}</div>
-    )}
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
 }
-
 
 export default App;
