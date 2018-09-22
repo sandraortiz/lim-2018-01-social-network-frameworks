a3b34a64326fc2a9f54640b785a9aa2f4dd5f46e
sandra ortiz cardenas <sandrarocio12oc@gmail.com>
2018-09-22 12:22:24 -0500

    probando registro con google

--- src/App.js
+++ src/App.js
@@ -1,17 +1,70 @@
+// asi exportarmos la estrucutura que queremos de lo contrario 
+// tendria que React.componet
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import firebase from 'firebase';
 import './App.css';
 
 class App extends Component {
-  render() {
+  constructor() {
+    super();
+    this.state = {
+    user : null
+  }
+
+  this.handleAuth  = this.handleAuth.bind(this);
+  this.handleLogout = this.handleLogout.bind(this);
+  this.renderLoginButton = this.renderLoginButton.bind(this);
+  }
+//CICLO DE VIDA SE DISPARA CUANDO EL COMPONENTE SE HA RENDERIZADO
+  componentWillMount() {
+    firebase.auth().onAuthStateChanged(user => {
+    //MODIFICAR EL ESTADO 
+        this.setState ({ user})
+        // User is signed in.
+
+    
+    });
+  }
+handleAuth() {
+const provider =  new firebase.auth.GoogleAuthProvider();
+firebase.auth().signInWithPopup(provider)
+.then(result =>console.log(`${result.user.email} inicio sesion`))
+.catch(error => console.log(`ERROR ${error.code} : ${error.message}`));
+}
+
+handleLogout() {
+  firebase.auth().signOut()
+  .then(result =>console.log(`${result.user.email} cerro sesion`))
+  .catch(error => console.log(`ERROR ${error.code} : ${error.message}`));
+}
+
+renderLoginButton () {
+  // si esta logueado 
+if(this.state.user){
+return(
+<div>
+  <img src = {this.state.user.photoURL}  alt= {this.state.displayName} />
+  <p> hola {this.state.user.displayName  } </p>
+  <button onClick = {this.handleLogout} > salir </button>
+</div>
+
+)
+}
+else {
+  return (
+           <button onClick= {this.handleAuth} > google</button>
+  )
+
+}
+}
+render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+  {this.renderLoginButton()}
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
+
+ firebase.initializeApp({
+    apiKey: "AIzaSyD33a72Urx8vLxoSDdJu0woInLXFQ3yq3M",
+    authDomain: "socialnetwork-165d4.firebaseapp.com",
+    databaseURL: "https://socialnetwork-165d4.firebaseio.com",
+    projectId: "socialnetwork-165d4",
+    storageBucket: "socialnetwork-165d4.appspot.com",
+    messagingSenderId: "544234037594"
+  });
+ 
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
