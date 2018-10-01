950912c56a5423c8c1738a63ea87e646514a9050
sandraortiz <rocio12vx@gmail.com>
2018-10-01 09:36:18 -0500

    Añadiendo mas componentes

--- src/App4.js
+++ src/App4.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import firebase from './firebase';
 import './App.css';
-import FileUpload from './FileUpload';
-
 class App extends Component {
   constructor () {
     super();
@@ -10,17 +8,16 @@ class App extends Component {
       email: '',
       password: '',
       user: null,
-      pictures: [] ,
- 
-    };
+      messages: [] 
+         };
 // LOGIN Y REGISTER
     this.login = this.login.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.signup = this.signup.bind(this);
 //
     this.handleAuth = this.handleAuth.bind(this);
-    this.handleUpload = this.handleUpload.bind(this);
-     this.handleAuthf  = this.handleAuthf.bind(this);
+    
+   this.handleAuthf  = this.handleAuthf.bind(this);
   }
 
   handleChange(e) {
@@ -43,28 +40,35 @@ class App extends Component {
       })
   }
   
- 
-
-
+  addMessage(e){
+    e.preventDefault();
+   
+    const currentUser = firebase.auth().currentUser;
+      const dbRef = firebase.database().ref().child('posts').push().key;
+      const record = {
+        image: currentUser.photoURL,
+          author: currentUser.displayName,
+          uid: currentUser.uid,
+        text: this.postContent.value,
+        keyposts : dbRef
+      }
+      const updates = {};
+      updates['/posts/' + dbRef] = record;
+      updates['/user-posts/' + currentUser.uid + '/' + dbRef] = record;
+      return firebase.database().ref().update(updates);
+   }
   componentWillMount () {
-    // Cada vez que el método 'onAuthStateChanged' se dispara, recibe un objeto (user)
-    // Lo que hacemos es actualizar el estado con el contenido de ese objeto.
-    // Si el usuario se ha autenticado, el objeto tiene información.
-    // Si no, el usuario es 'null'
-    firebase.auth().onAuthStateChanged(user => {
+        firebase.auth().onAuthStateChanged(user => {
       this.setState({ user });
      this.userUpload({user})
-      
-    });
-
-    firebase.database().ref('pictures').on('child_added', snapshot => {
-      this.setState({
-        pictures: this.state.pictures.concat(snapshot.val())
       });
-    });
-
-
-  }
+     firebase.database().ref('posts').orderByKey().on('child_added', snapshot => {
+      let message = { text:snapshot.val().text , id: snapshot.keyposts , author:snapshot.val().author };
+      this.setState({ messages: [message].concat(this.state.messages)
+      });
+     })
+    
+    }
 
   handleAuth () {
     const provider = new firebase.auth.GoogleAuthProvider();
@@ -87,7 +91,6 @@ class App extends Component {
       .then(result => console.log(`${result.user.email} ha iniciado sesión`))
       .catch(error => console.log(`Error ${error.code}: ${error.message}`));
   }
-  
   userUpload() {
     firebase.database().ref('users/' + this.state.user.uid).set({
       username: this.state.user.displayName ,
@@ -95,24 +98,7 @@ class App extends Component {
     
   });
   }
-   
-  handleUpload () {
   
-     const dbRef = firebase.database().ref().child('pictures').push().key;
-      const record = {
-        photoURL: this.state.user.photoURL,
-        displayName: this.state.user.displayName,
-      body:this.state.posts ,
-        id :  this.state.user.uid 
-      }
-      const updates = {};
-      updates['/pictures/' + dbRef] = record;
-      updates['/user-pictures/' + this.state.user.uid + '/' + dbRef] = record;
-      return firebase.database().ref().update(updates);
-    
-   
-  }
-
   renderLoginButton () {
     if (!this.state.user) {
       return (
@@ -145,36 +131,30 @@ class App extends Component {
           <button onClick={this.handleLogout} className="App-btn">
             Salir
           </button>
-         <userUpload />
-          <FileUpload onUpload={this.handleUpload}/>
+          <userUpload />
+          <form onSubmit={this.addMessage.bind(this)}>
+    <input type="text" ref={ post => this.postContent = post }/>
+    <input type="submit"/>
+    
+     { 
+       this.state.messages.map( message =>
+        <div>
+             <p key={message.id}>{message.author}</p>     
+    <p key={message.id}>{message.text}</p>   
+           </div>
+        )
+     }
+  
+   </form>
 
-          {
-            this.state.pictures.map(picture => (
-              <div className="App-card">
-                <figure className="App-card-image">
-                  <img className="App-card-avatar"  src= {picture.image} alt = { "figuresuser"} />
-                
-                    <img className="App-card-avatar" src={picture.photoURL} alt={`${picture.displayName}`} />
-                    <span className="App-card-name">{picture.displayName}</span>
-
-                </figure>
-              </div>
-            )).reverse()
-          }
-
-        </div>
+          </div>
 
       );
     }
   }
 
   render() {
-    return (
-      <div className="App">
-       
-        { this.renderLoginButton() }
-      </div>
-    );
+    return ( <div className="App"> { this.renderLoginButton() } </div>);
   }
 }
 
