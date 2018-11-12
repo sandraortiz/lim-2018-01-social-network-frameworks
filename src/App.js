17715ff5cbdd32b2f90d26a644690e419aa3b8cb
sandraortiz <rocio12vx@gmail.com>
2018-11-12 17:22:42 -0500

    creando componentes

--- src/App.js
+++ src/App.js
@@ -1,48 +1,16 @@
 import React, { Component } from 'react';
-import firebase from './firebase';
 
 import './App.css';
-import Home from './Home'
-import Wall from './Wall'
 class App extends Component {
-  constructor () {
-    super();
-    this.state = {
-      
-      user: null,
-      
-         };
-// LOGIN Y REGISTER
-this.authListener = this.authListener.bind(this);
+
+  
+  render() {
+    return (
+ <p>hola</p>
+    );
+  }
+
+
 }
 
-componentWillMount () {
-  this.authListener();
-      
-    }
-    authListener() {
-      firebase.auth().onAuthStateChanged((user) => {
-        console.log(user);
-        if (user) {
-      
-       this.setState({ user });
-
-         firebase.database().ref('users/' + user.uid).set({
-        username: this.state.user.displayName ,
-        email: this.state.user.email 
-        });
-           
-         } 
-         else {
-            this.setState({ user: null });
-        }
-      });
-    }
-   
-    render() {
-      return (
-       <div>{this.state.user ?  ( <Wall/>) : (<Home />)}</div>
-      )}
-  }
-  export default App;
-
+export default App;
