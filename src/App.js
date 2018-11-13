ed7ea2854f98f0d19eaf67d43af38293fe5519b7
ELEUTERIA <julia.sucasacai@gmail.com>
2018-11-13 15:38:01 -0600

    state y botones

--- src/App.js
+++ src/App.js
@@ -2,12 +2,38 @@ import React, { Component } from 'react';
 
 import './App.css';
 class App extends Component {
-
+    constructor(){
+      super();
+      this.state={
+        user: false
+      }
+    }
+  loginFunction=()=>{
+    this.setState({user:true})
+  } 
+  
+  logoutFunction=()=>{
+    this.setState({user:false})
+  }
   
   render() {
-    return (
- <p>hola</p>
-    );
+    if(this.state.user===false){
+      return (
+        <div>
+          <button onClick={this.loginFunction}>Ingresa</button>
+        <button>Registrate</button>
+        </div>
+        
+           )
+    }else if(this.state.user=== true){
+      return (
+        <div>
+          <h1>Estas en homeeeee</h1>
+          <button  onClick={this.logoutFunction}>Cierra sesión</button>
+        </div>
+        
+           )
+    }
   }
 
 
