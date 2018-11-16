f99510ea463a62d79ed0562c94c4c65392716f33
ELEUTERIA <julia.sucasacai@gmail.com>
2018-11-15 22:02:53 -0600

    login con email, facebook y google

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import firebase from 'firebase'
 
 import './App.css';
 class App extends Component {
@@ -8,9 +9,60 @@ class App extends Component {
         user: false
       }
     }
+  loginGoogle=()=>{
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+      .then(result => {
+        console.log(`${result.user.email} ha iniciado sesión`);
+        this.setState({user:true,
+                      userEmail: `${result.user.email}` })
+
+      })
+      .catch(error=> console.log(`error ${error.code}: ${error.message }`));
+      
+  }  
+  loginFacebook=()=>{
+    const provider = new firebase.auth.FacebookAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+      .then(result => {
+        console.log(`${result.user.email} ha iniciado sesión`);
+        this.setState({user:true,
+                      userEmail: `${result.user.email}` })
+
+      })
+      .catch(error=> console.log(`error ${error.code}: ${error.message }`));
+  }
   loginFunction=()=>{
-    this.setState({user:true})
+    const email= document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((result)=>{
+      this.setState({user:true})
+    })
+    .catch((error)=> {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ...
+    });
+    alert(email, password)
   } 
+
+  loginRegister=()=>{
+    const email= document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    
+    .catch((error)=> {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      
+      // ...
+    });
+  }
   
   logoutFunction=()=>{
     this.setState({user:false})
@@ -20,8 +72,12 @@ class App extends Component {
     if(this.state.user===false){
       return (
         <div>
+          <input type="email" placeholder="Email" id="email"/>
+          <input type="password" placeholder="password" id="password"/>
           <button onClick={this.loginFunction}>Ingresa</button>
-        <button>Registrate</button>
+          <button onClick={this.loginGoogle}>Google</button>
+          <button onClick={this.loginFacebook}>Facebook</button>
+        <button onClick={this.loginRegister}>Registrate</button>
         </div>
         
            )
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import firebase from 'firebase'
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import "font-awesome/css/font-awesome.min.css";
-import "bootstrap-css-only/css/bootstrap.min.css";
-import "mdbreact/dist/css/mdb.css";
+ // Initialize Firebase
+  firebase.initializeApp({
+    apiKey: "AIzaSyATtSJ8rOly0GPDBfUAF9zZCxWcttwWEBk",
+    authDomain: "social-react-4c9d9.firebaseapp.com",
+    databaseURL: "https://social-react-4c9d9.firebaseio.com",
+    projectId: "social-react-4c9d9",
+    storageBucket: "social-react-4c9d9.appspot.com",
+    messagingSenderId: "1042389204283"
+  });
 ReactDOM.render(<App/>, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
